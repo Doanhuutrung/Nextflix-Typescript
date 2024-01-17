e71346c26f08dbd56d72642f6fc3abc6ecfaf3fd
Huu Trung <huutrungdoan22202@gmail.com>
2024-01-17 15:28:16 +0700

    setup recently added movie section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,15 @@
 import MovieVideo from "../components/MovieVideo";
 import Navbar from "../components/Navbar";
+import RecentlyAdded from "../components/RecentlyAdded";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="p-5 lg:p-0">
       <MovieVideo />
+      <h1 className="text-3xl font-bold">Recently Addded</h1>
+      <RecentlyAdded />
     </div>
   );
 }
+
+//@media (min-width: 1024px) is lg
